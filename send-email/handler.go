2f51bfc5031b8525f0726f026bbfcfcb9d1e2d3e
Dakota Lillie <dakota.lillie@icloud.com>
2020-12-01 00:46:45 -0500

    Add cors headers

--- send-email/handler.go
+++ send-email/handler.go
@@ -10,15 +10,20 @@ type handler struct {
 }
 
 func (h *handler) Run(request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
+	headers := make(map[string]string)
+	headers["Access-Control-Allow-Headers"] = "Content-Type"
+	headers["Access-Control-Allow-Origin"] = "*"
+	headers["Access-Control-Allow-Methods"] = "OPTIONS,POST"
+
 	config, err := NewEmailConfig(request.Body, h.ssmSvc)
 	if codedError, ok := err.(*codedError); ok {
-		return events.APIGatewayProxyResponse{StatusCode: codedError.Code, Body: codedError.Error()}, codedError
+		return events.APIGatewayProxyResponse{StatusCode: codedError.Code, Headers: headers, Body: codedError.Error()}, codedError
 	}
 
 	mailer := Mailer{config}
 	if err = mailer.Send(); err != nil {
-		return events.APIGatewayProxyResponse{StatusCode: 502, Body: err.Error()}, err
+		return events.APIGatewayProxyResponse{StatusCode: 502, Headers: headers, Body: err.Error()}, err
 	}
 
-	return events.APIGatewayProxyResponse{StatusCode: 200}, nil
+	return events.APIGatewayProxyResponse{StatusCode: 200, Headers: headers}, nil
 }
