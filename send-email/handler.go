50510aa825dc007a8856f92131db42c831c6101d
Dakota Lillie <dakota.lillie@icloud.com>
2020-12-04 00:06:15 -0500

    Update response headers

--- send-email/handler.go
+++ send-email/handler.go
@@ -13,17 +13,25 @@ func (h *handler) Run(request events.APIGatewayProxyRequest) (events.APIGatewayP
 	// These values were copied from here: https://docs.aws.amazon.com/apigateway/latest/developerguide/how-to-cors.html
 	headers := make(map[string]string)
 	headers["Access-Control-Allow-Headers"] = "Content-Type"
-	headers["Access-Control-Allow-Origin"] = "dakotadacoda.com"
 	headers["Access-Control-Allow-Methods"] = "OPTIONS,POST"
+	headers["Content-Type"] = "text/html; charset=UTF-8"
+
+	allowedOrigins := []string{"https://dakotadacoda.com", "https://www.dakotadacoda.com"}
+	for _, origin := range allowedOrigins {
+		if request.Headers["Origin"] == origin {
+			headers["Access-Control-Allow-Origin"] = origin
+			break
+		}
+	}
 
 	config, err := NewEmailConfig(request.Body, h.ssmSvc)
 	if codedError, ok := err.(*codedError); ok {
-		return events.APIGatewayProxyResponse{StatusCode: codedError.Code, Headers: headers, Body: codedError.Error()}, codedError
+		return events.APIGatewayProxyResponse{StatusCode: codedError.Code, Headers: headers, Body: codedError.Error()}, nil
 	}
 
 	mailer := Mailer{config}
 	if err = mailer.Send(); err != nil {
-		return events.APIGatewayProxyResponse{StatusCode: 502, Headers: headers, Body: err.Error()}, err
+		return events.APIGatewayProxyResponse{StatusCode: 502, Headers: headers, Body: err.Error()}, nil
 	}
 
 	return events.APIGatewayProxyResponse{StatusCode: 200, Headers: headers}, nil
